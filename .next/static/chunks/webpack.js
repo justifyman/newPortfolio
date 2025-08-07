30dce161dd3ca2ecf62d200f5115a1c74a8f6ca3
justify <143286685+justifyman@users.noreply.github.com>
2025-08-07 16:20:24 +0400

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e559b6d6ff792c98"; }
+/******/ 		__webpack_require__.h = function() { return "52b84a2d454fe63e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -50,7 +50,7 @@ export default function Footer() {
         >
           <Button variant={"outline"}>
             <MailIcon className="h-4 w-4 md:mr-2" />
-            <span className="hidden md:flex">justifydev@proton.me</span>
+            <span className="hidden md:flex">manjustify@gmail.com</span>
           </Button>
         </Link>
       </div>
